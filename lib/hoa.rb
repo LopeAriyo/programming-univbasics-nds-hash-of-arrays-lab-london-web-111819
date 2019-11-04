bd57fe136c38421d5ce6e3c537fde024f21b193e
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-04 21:16:15 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,6 +9,10 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
 
-  show.push(name)
-  p show
+  #show is assigned to :chipmunks but doesn't currently display the Array
+  # name is assigned to Dave
+  # we want to add Dave to the end of the chipmunks array
+
+  BASE_HOA[show].push(name)
+  p BASE_HOA[show]
 end
